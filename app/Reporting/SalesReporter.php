9f9403c64ddf0878c4889e484286133f47d9be82
Ryan Ebbers <ebbers.seo@gmail.com>
2018-10-11 16:38:11 -0400

    Removed Authentication from SalesReporter class
    Authentication is not the responsibilty of the class, and can be done within the controller.

--- app/Reporting/SalesReporter.php
+++ app/Reporting/SalesReporter.php
@@ -11,9 +11,6 @@ class SalesReporter extends Model
 {
     public function between($startDate, $endDate)
     {
-        // perform authentication
-        if (! Auth::check()) throw new Exception('Authentication required for reporting.');
-
         // get sales from db
         $sales = $this->queryDBForSalesBetween($startDate, $endDate);
 
