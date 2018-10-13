aa77726d0dc9fd79069cf6a43ac9bef4ff7ef5c6
Ryan Ebbers <ebbers.seo@gmail.com>
2018-10-13 13:47:10 -0400

    ISP Example
    Created a WorkerInterface with the work() and sleep() functions.
    
    Changed Worker to HumanWorker class, which implements the WorkerInterface.
    
    But now we need to manage AndroidWorker objects.

--- app/Lessons/InterfaceSegregation/Example.php
+++ app/Lessons/InterfaceSegregation/Example.php
@@ -6,16 +6,23 @@ namespace App\Lessons\InterfaceSegregation;
  * Imagine we are in the world of Star Trek.
  */
 
-class Worker
+interface WorkerInterface
+{
+    public function work();
+
+    public function sleep();
+}
+
+class HumanWorker implements WorkerInterface
 {
     public function work()
     {
-        //
+        // TODO: Implement work() method.
     }
 
     public function sleep()
     {
-        //
+        // TODO: Implement sleep() method.
     }
 }
 
